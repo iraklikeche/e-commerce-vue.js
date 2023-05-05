8c0dba485308db9a6befefb359577eefcbaff63c
iraklikeche <irakli.keche@yahoo.com>
2023-05-05 21:15:08 +0400

    hero-section

---
+++ src/components/Home.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <section class="header">
+      <a href=""><img src="../assets/img/logo.png" alt="" /></a>
+      <div>
+        <ul class="navbar">
+          <li><a class="active" href="#">Home</a></li>
+          <li><a href="#">Shop</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li>
+            <a href="cart.html"
+              ><i id="lg-bag" class="fa-solid fa-cart-shopping"></i
+            ></a>
+          </li>
+          <i id="close" class="fa-solid fa-xmark"></i>
+        </ul>
+      </div>
+      <div class="mobile">
+        <a href="cart.html"><i class="fa-solid fa-cart-shopping"></i></a>
+        <i class="fa fa-solid fa-bars"></i>
+      </div>
+    </section>
+
+    <!-- ------------------ HERO -------------------- -->
+    <section class="hero">
+      <h4>Trade-in-offer</h4>
+      <h2>Super value deals</h2>
+      <h1>On all products</h1>
+      <p>Save more with coupons & up to 70% off!</p>
+      <button>Shop Now</button>
+    </section>
+    <!-- ------------------ FEATURE -------------------- -->
+  </div>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&family=Montserrat&family=Poppins:wght@100;200;300;400;600;700&family=Ubuntu:ital,wght@0,300;0,400;1,300&display=swap");
+
+h1 {
+  font-size: 64px;
+  line-height: 84px;
+  color: #222;
+}
+
+h2 {
+  font-size: 52px;
+  line-height: 54px;
+  color: #222;
+}
+
+h4 {
+  font-size: 32px;
+  color: #222;
+}
+
+h6 {
+  font-weight: 700;
+  font-size: 12px;
+}
+
+p {
+  font-size: 16px;
+  color: #465b52;
+  margin: 15px 0 20px 0;
+}
+
+.header {
+  display: flex;
+  text-align: center;
+  justify-content: space-between;
+  padding: 20px 80px;
+  background-color: #e3e6f3;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.06);
+  z-index: 999;
+  position: sticky;
+  top: 0;
+  left: 0;
+}
+
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.navbar li {
+  list-style: none;
+  padding: 0 20px;
+  position: relative;
+}
+
+.navbar li a {
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: 600;
+  color: #1a1a1a;
+  transition: 0.3s ease;
+}
+
+.navbar li a:hover,
+.navbar li a.active {
+  color: #088178;
+}
+
+.navbar li a.active:after,
+.navbar li a:hover::after {
+  content: "";
+  width: 30%;
+  height: 2px;
+  background-color: #088178;
+  position: absolute;
+  bottom: -4px;
+  left: 20px;
+}
+
+.mobile {
+  display: none;
+  align-items: center;
+}
+
+#close {
+  display: none;
+}
+
+/* ----------------- HERO ------------------ */
+
+.hero {
+  background-image: url(../assets/img/hero4.png);
+  height: 90vh;
+  background-size: cover;
+  background-position: top 25% right 0;
+  padding: 0 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+}
+
+.hero h4 {
+  padding-bottom: 15px;
+}
+
+.hero h1 {
+  color: #088178;
+}
+
+.hero button {
+  background-image: url(../assets/img/button.png);
+  background-color: transparent;
+  color: #088178;
+  border: 0;
+  padding: 14px 80px 14px 65px;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  font-weight: 700;
+  font-size: 15px;
+}
+</style>
