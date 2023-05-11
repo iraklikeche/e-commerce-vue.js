519239c42055b19c164694aa81be6078d43306c2
iraklikeche <irakli.keche@yahoo.com>
2023-05-11 15:28:32 +0400

    contact page icons

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -46,14 +46,41 @@ const people = [
       <h6>GET IN TOUCH</h6>
       <h2>Visit one of our agency locations or contact us today</h2>
       <h5>Head Office</h5>
-      <p>
-        <i class="fa-solid fa-map"></i>56 Glassford Street Glasgow G11UL New
-        York
+      <p class="icon-box">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+          class="icon"
+        >
+          <path
+            d="M384 476.1L192 421.2V35.9L384 90.8V476.1zm32-1.2V88.4L543.1 37.5c15.8-6.3 32.9 5.3 32.9 22.3V394.6c0 9.8-6 18.6-15.1 22.3L416 474.8zM15.1 95.1L160 37.2V423.6L32.9 474.5C17.1 480.8 0 469.2 0 452.2V117.4c0-9.8 6-18.6 15.1-22.3z"
+          />
+        </svg>
+        56 Glassford Street Glasgow G11UL New York
       </p>
-      <p><i class="fa-solid fa-envelope"></i>contact@example.com</p>
-      <p><i class="fa-solid fa-phone"></i>contact@example.com</p>
-      <p>
-        <i class="fa-regular fa-clock"></i>Monday to Saturday: 09.00 to 16.00
+      <p class="icon-box">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          class="icon"
+        >
+          <path
+            d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
+          />
+        </svg>
+        contact@example.com
+      </p>
+      <p class="icon-box">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          class="icon"
+        >
+          <path
+            d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
+          />
+        </svg>
+        Monday to Saturday: 09.00 to 16.00
       </p>
     </div>
     <div class="map">
@@ -219,5 +246,16 @@ const people = [
   font-size: 15px;
   font-weight: 600;
   color: #000;
+}
+
+.icon {
+  width: 20px;
+  height: 20px;
+}
+
+.icon-box {
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
 </style>
